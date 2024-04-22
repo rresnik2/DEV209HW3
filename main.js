af583dba520dac478039c81c281d071882d7f7e9
rresnik2 <rresnik2@gmail.com>
2024-04-21 21:32:13 -0700

    Hides list before user input

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
+  let outputList = document.getElementById("movieList");
+  outputList.style.display = "none";
+  var movieList = [];
+  
   function sortTitle() {
     movieList.sort(function(a, b) {
       let x = a.title.toLowerCase();
@@ -14,8 +17,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
   }
-  var movieList = [];
-  
+   
   document.getElementById("addMovie").addEventListener("click", function() {
     console.log(document.getElementById("year").value);
   console.log(document.getElementById("rating").value);
@@ -39,8 +41,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
       oMovies += "<li>Movie:\t" + i.title + ",\tYear:\t" + i.year + ",\tRating:\t"+ i.rating + "</li>";
     }
     oMovies += "</ul>";
-    document.getElementById("movieList").innerHTML = oMovies;
-
+    outputList.innerHTML = oMovies;
+    outputList.getElementById("movieList").style.display = "block";
   });
 
 
